0c37fab745160433204e0e522ec665e804c1ac04
@sl2 <omarsanluis50@gmail.com>
2026-05-18 18:01:26 -0600

    Fix | Actividad 1: User class

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,40 @@
-public function roles()
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+#[Fillable(['name', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
+class User extends Authenticatable
 {
-return $this->belongsToMany(Role::class, 'user_roles');
-}
-public function hasRole($role)
-{
-return $this->roles->where('name', $role)->isNotEmpty();
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable;
+
+    public function roles()
+    {
+    return $this->belongsToMany(Role::class, 'user_roles');
+    }
+    public function hasRole($role)
+    {
+    return $this->roles->where('name', $role)->isNotEmpty();
+    }
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
